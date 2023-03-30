bed2b8c9e30155a3769e17c9a835f0ef967809b0
Christopher Narayanan <93202854+Queentaker@users.noreply.github.com>
2023-03-30 21:35:24 +0200

    fix s1: fixing tests so that build works

--- src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
@@ -25,7 +25,7 @@ public class DTOMapperTest {
     User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
 
     // check content
-    assertEquals(userPostDTO.getPassword(), user.getName());
+
     assertEquals(userPostDTO.getUsername(), user.getUsername());
   }
 
@@ -33,7 +33,7 @@ public class DTOMapperTest {
   public void testGetUser_fromUser_toUserGetDTO_success() {
     // create User
     User user = new User();
-    user.setName("Firstname Lastname");
+
     user.setUsername("firstname@lastname");
     user.setStatus(UserStatus.OFFLINE);
     user.setToken("1");
@@ -43,7 +43,7 @@ public class DTOMapperTest {
 
     // check content
     assertEquals(user.getId(), userGetDTO.getId());
-    assertEquals(user.getName(), userGetDTO.getName());
+
     assertEquals(user.getUsername(), userGetDTO.getUsername());
     assertEquals(user.getStatus(), userGetDTO.getStatus());
   }
