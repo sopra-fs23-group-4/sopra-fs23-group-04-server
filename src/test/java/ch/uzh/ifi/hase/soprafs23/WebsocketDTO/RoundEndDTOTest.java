03e33b0bcc5bb0b18aa5a0aa7aefb127f0878632
Christopher Narayanan <93202854+Queentaker@users.noreply.github.com>
2023-05-22 15:19:05 +0200

    fixing tests that failed so that deploy works

--- src/test/java/ch/uzh/ifi/hase/soprafs23/WebsocketDTO/RoundEndDTOTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/WebsocketDTO/RoundEndDTOTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class RoundEndDTOTest {
     @Test
     void getType_shouldReturnCorrectType() {
-        Assertions.assertEquals("roundEnd", RoundEndDTO.TYPE);
+        Assertions.assertEquals("roundEnd", RoundEndDTO.type);
     }
 
 }
