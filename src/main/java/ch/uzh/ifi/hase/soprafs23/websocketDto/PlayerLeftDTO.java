e22c64a97727a5ec3a3057fe95bd027e3b41cd8a
Christopher Narayanan <93202854+Queentaker@users.noreply.github.com>
2023-05-22 18:21:08 +0200

    #280

--- src/main/java/ch/uzh/ifi/hase/soprafs23/websocketDto/PlayerLeftDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websocketDto/PlayerLeftDTO.java
@@ -3,6 +3,10 @@ package ch.uzh.ifi.hase.soprafs23.websocketDto;
 public class PlayerLeftDTO {
     public static final String type="playerLeft";
 
+    public String getType(){
+        return type;
+    }
+
     private String username;
 
     public String getUsername() {
